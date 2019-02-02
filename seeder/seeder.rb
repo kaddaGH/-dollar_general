bba5d0fd17c9289ef4cac1cc22ee8a828956df88
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-02-02 18:08:01 +0100

    Update scraper.

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,21 +1,11 @@
 # Taxonomy
 #
 #
-headers = {
 
-    'Host' => 'www.dollargeneral.com',
-    'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-    'Accept - Language ' => 'fr, fr - FR; q = 0.8, en - US; q = 0.5, en; q = 0.3 ',
-    'Accept-Encoding' => 'gzip, deflate, br',
-    'DNT ' => ' 1 ',
-    'Connection' => 'keep-alive',
-   'Upgrade-Insecure-Requests' => '1'
-}
 pages << {
     page_type: 'products_listing',
     url: 'https://www.dollargeneral.com/food/beverages/energy-drinks.html',
     fetch_type: 'fullbrowser',
-    headers:headers,
     vars: {
         input_type: 'taxonomy',
         search_term: '-',
@@ -28,7 +18,6 @@ pages << {
       page_type: 'products_listing',
       url: "https://www.dollargeneral.com/catalogsearch/result/?q=#{search_term.gsub(' ', '+')}",
       fetch_type: 'fullbrowser',
-      headers:headers,
       vars: {
           input_type: 'Search',
           search_term: search_term,
