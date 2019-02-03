f40496cec030b83659b5cef454e1e6aec7233e00
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-02-03 01:16:15 +0100

    Update seeder.

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,12 +1,16 @@
 # Taxonomy
-pages << {
-    page_type: 'products_listing',
-    url: 'https://www.dollargeneral.com/food/beverages/energy-drinks.html',
-    fetch_type: 'browser',
-    vars: {
-        input_type: 'taxonomy',
-        search_term: '-',
-        page: 1
-    }
-}
+#
+#
 
+['Red bull', 'RedBull', 'Energy Drink', 'Energy Drinks'].each do |search_term|
+  pages << {
+      page_type: 'products_listing',
+      url: "https://www.dollargeneral.com/catalogsearch/result/?q=#{search_term.gsub(' ', '+')}",
+      fetch_type: 'browser',
+      vars: {
+          input_type: 'Search',
+          search_term: search_term,
+          page: 1
+      }
+  }
+end
