0f93af143e5c180ddcabc4b2dbbfd51dd717042f
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-02-03 22:06:32 +0100

    Update scraper.

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,10 +1,22 @@
 require './lib/headers'
 # Taxonomy
+#
+
+phantomjscloud_api_key = 'ak-4grtj-aha4b-1vh2t-be9b8-psnv0'
+phantomjscloud_api_url = 'http://PhantomJScloud.com/api/browser/v2/'+phantomjscloud_api_key+'/'
+headers = {'content-type'=>'application/json'}
+
+request_body={
+    "url"=>"https://www.dollargeneral.com/food/beverages/energy-drinks.html",
+    "renderType"=>"html"
+}
+
  pages << {
    page_type: 'listing',
-   url: 'https://www.dollargeneral.com/food/beverages/energy-drinks.html',
-   fetch_type: 'browser',
-   headers: ReqHeaders::SEARCH_PAGE_HEADER_REQ,
+   method:'POST',
+   url: phantomjscloud_api_url,
+   headers:headers,
+   body:request_body.to_json,
    vars: {
      input_type: 'taxonomy',
      search_term: '-',
@@ -13,11 +25,17 @@ require './lib/headers'
  }
 
 ['Red bull', 'RedBull', 'Energy Drink', 'Energy Drinks'].each do |search_term|
+
+  request_body={
+      "url"=>"https://www.dollargeneral.com/catalogsearch/result/?q=#{search_term}",
+      "renderType"=>"html"
+  }
   pages << {
       page_type: 'products_listing',
-      url: "https://www.dollargeneral.com/catalogsearch/result/?q=#{search_term}",
-      fetch_type: 'browser',
-      headers: ReqHeaders::SEARCH_PAGE_HEADER_REQ,
+      method:'POST',
+      url: phantomjscloud_api_url,
+      headers:headers,
+      body:request_body.to_json,
       vars: {
           input_type: 'Search',
           search_term: search_term,
