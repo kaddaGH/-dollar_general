df798b0dc8bd85514b3722e39fcd5c290caed20e
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-02-28 17:49:05 +0100

    Update scraper.

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,11 +1,21 @@
 render_api_url = 'https://api.prerender.com/render?token=v5FvYt7VfAo0W4dIO0ly&url='
+pages << {
+    page_type: 'products_listing',
+    method:'GET',
+    url: render_api_url+"https://www.dollargeneral.com/food/beverages/energy-drinks.html",
 
+    vars: {
+        input_type: 'taxonomy',
+        search_term: '-',
+        page: 1
+    }
+}
 
-['Energy Drink'].each do |search_term|
+['Red bull', 'RedBull', 'Energy Drink', 'Energy Drinks'].each do |search_term|
   pages << {
       page_type: 'products_listing',
       method:'GET',
-      url: render_api_url+"https://www.dollargeneral.com/catalogsearch/result/?q=#{search_term}&p=5",
+      url: render_api_url+"https://www.dollargeneral.com/catalogsearch/result/?q=#{search_term}",
       vars: {
           input_type: 'Search',
           search_term: search_term,
